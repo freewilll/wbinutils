e135a70e28bc0f6da75c61a616fd380c36282c5f
Will Angenent <will.angenent@gmail.com>
2025-05-12 17:12:48 +0100

    Moved all layout code to its own file

---
+++ include/wld/layout.h
@@ -0,0 +1,11 @@
+#ifndef _LAYOUT_H
+#define _LAYOUT_H
+
+void layout_input_sections(RwElfFile *output_elf_file, List *input_elf_files);
+void layout_leftover_sections(RwElfFile *output_elf_file, List *input_elf_files);
+uint64_t layout_output_segments_and_sections(RwElfFile *output_elf_file);
+void layout_leftover_output_segments_and_sections(RwElfFile *output_elf_file, List *input_elf_files, uint64_t offset);
+void remove_empty_sections(RwElfFile *output_elf_file);
+void make_elf_section_headers(RwElfFile *output_elf_file);
+
+#endif
